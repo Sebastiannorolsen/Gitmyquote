487fb21515d4ca5303723756c321ce7921626ed7
Sebastiannorolsen <sebaso15@uia.no>
2022-01-27 11:25:18 +0100

    gitinit

---
+++ myquote.go
@@ -0,0 +1,21 @@
+package myquote
+
+import "rsc.io/quote"
+
+
+func Hello() string {
+	return quote.Hello()
+}
+
+func Glass() string {
+	return quote.Glass()
+}
+	
+func Go() string {
+        return quote.Go()
+}
+
+func Opt() string {
+        return quote.Opt()
+}
+
